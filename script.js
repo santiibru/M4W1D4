581fa608ca9aa07518a3ce29ebe026c7d6b9939b
Santiago Bruno <santiago.p.bruno@gmail.com>
2024-02-05 17:00:50 +0100

    index.html

---
+++ script.js
@@ -0,0 +1,18 @@
+//IN AGGIORNAMENTO...
+
+
+
+
+//let eminemCovers = document.getElementById("eminemSection");
+// let eminemData = [];
+// fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+// // // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+// .then((response) => response.json()) // Abbiamo una promise restituita qui
+// .then((json) => eminemData.push(json.data[0].album.cover))      
+//     .catch((err) => console.log("Error detected: ", err));
+// console.log(eminemData);
+
+// let myImg = document.createElement("img");
+
+
+
